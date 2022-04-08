c3490c5b9b787342f4f3d5c2d2c56e6dbfbc53c7
esterfrazao <tafnisbonbon@gmail.com>
2022-04-08 00:43:54 -0300

    estilizando pagina inicial

--- src/InicialPage.jsx
+++ src/InicialPage.jsx
@@ -1,10 +1,22 @@
 import "./inicialPage.css";
+import capa from "./capa.svg";
+import logo from "./Nu-Kenzie-Branco.svg";
 
 function InicialPage({ callback }) {
   return (
-    <>
-      <button onClick={callback}>Iniciar</button>
-    </>
+    <section className="inicialPage">
+      <div className="containerPressentation">
+        <div className="presentation">
+          <img alt="logo" src={logo} className="logo" />
+          <h1 className="slogan">Centralize o controle das suas finanças</h1>
+          <p className="presentationDetail">de forma rápida e segura</p>
+          <button className="buttonGoToMainPage" onClick={callback}>
+            Iniciar
+          </button>
+        </div>
+        <img alt="imagem de capa" src={capa} className="capa" />
+      </div>
+    </section>
   );
 }
 
